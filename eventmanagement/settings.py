9a88377bf59ad01306789fc9d02a73dd51be9e61
Kabya <sb.kabya@gmail.com>
2026-02-15 22:11:23 +0600

    Add requirements.txt and update settings.py

--- eventmanagement/settings.py
+++ eventmanagement/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-n%bhec)zgu!!k6(ky3p1k+ibs48bl*^&)lj&=*44p*q_v%+qep
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
 
 # Application definition
 
